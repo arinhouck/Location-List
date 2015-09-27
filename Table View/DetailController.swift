5a0e3ee3d767f6aa65774dd76f8f43b52746ff80
Arin Houck <arinhouck@gmail.com>
2015-09-26 18:47:22 -0700

    finished lab 4, maps

--- Table View/DetailController.swift
+++ Table View/DetailController.swift
@@ -7,27 +7,70 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailController: UIViewController {
+    
     var imageName = String()
     var locationName = String()
     var locationDesc = String()
-    
-    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var detailTitle: UILabel!
     @IBOutlet weak var detailDescription: UILabel!
     
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+
+    @IBOutlet weak var segmentedMapType: UISegmentedControl!
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageView.image = UIImage(named: "map-" + self.imageName)
         detailTitle.text = self.locationName
         detailDescription.text = self.locationDesc
+        markAddress(self.locationName)
+        
+        // Titles for Segemented Button
+        segmentedMapType.setTitle("Standard", forSegmentAtIndex: 0)
+        segmentedMapType.setTitle("Satellite", forSegmentAtIndex: 1)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // Action to control segmentented UI
+    @IBAction func mapTypeChanged(sender: UISegmentedControl) {
+        switch segmentedMapType.selectedSegmentIndex {
+            case 0:
+                map.mapType = MKMapType.Standard
+            case 1:
+               map.mapType = MKMapType.Satellite
+            default:
+            break;
+        }
+    }
+    
+    // Sets address with geocoder and fills in lat,lon in text fields
+    func markAddress(sender: AnyObject) {
+        let address = ( sender as! NSString)
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(address as String) { (placemarks, error) -> Void in
+            if let placemark = placemarks?[0] {
+                let span = MKCoordinateSpanMake(2, 2)
+                let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
+                self.map.setRegion(region, animated: true)
+                let ani = MKPointAnnotation()
+                ani.coordinate = placemark.location!.coordinate
+                self.latitudeTextField.text = ani.coordinate.latitude.description
+                self.longitudeTextField.text = ani.coordinate.longitude.description
+                ani.title = self.locationName
+                
+                self.map.addAnnotation(ani)
+            }
+        }
+
     }
     
     
